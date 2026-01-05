f80408f4b4af02ade1e3bb12524fef180fcbf80e
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2026-01-05 12:08:14 +0800

    fix : importing Icon

--- src/data/achievement.jsx
+++ src/data/achievement.jsx
@@ -1,3 +1,5 @@
+import { Award, Briefcase, Target, Users } from "lucide-react";
+
 export const achievements = [
   { icon: <Briefcase size={20} />, text: "CEO & Founder CV Impost Media Indonesia" },
   { icon: <Users size={20} />, text: "Memimpin puluhan karyawan & tim profesional" },
